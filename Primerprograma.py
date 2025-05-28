7127d4a916739e0c82013cd6844587cb729bd3a9
EdgarOrdonez04 <eordonez@uach.mx>
2025-05-28 17:29:06 -0600

    Update Primerprograma.py

--- Primerprograma.py
+++ Primerprograma.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Multiplicación de numeros")
-# num1 = st.input("Dame el primer numero")
-# num2 = st.input("Dame el segundo numero")
-# resultado=num1 * num2 
+num1 = st.input("Dame el primer numero")
+num2 = st.input("Dame el segundo numero")
+resultado=num1 * num2 
 st.write("El Resultado es: ")
