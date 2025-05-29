a3f6416587d2567e7a907c946bdc64f323f67359
EdgarOrdonez04 <eordonez@uach.mx>
2025-05-29 16:06:29 -0600

    Update app16.py

--- app16.py
+++ app16.py
@@ -9,11 +9,11 @@ openai_api_key = st.secrets["api_key"]
 client = OpenAI(api_key=openai_api_key)
 
 prompt = st.chat_input("¿De que hablaremos hoy?")
-#if prompt==None:
-#   st.stop()
+if prompt==None:
+   st.stop()
 
-#with st.chat_message("user"):
-#   st.markdown(prompt)
+with st.chat_message("abatar = 🦖"):
+   st.markdown(prompt)
 
 # Generate a response using the OpenAI API.
 
