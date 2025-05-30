625f53e23efe254d566f8d9a65f4144d36304d4b
EdgarOrdonez04 <eordonez@uach.mx>
2025-05-29 18:24:34 -0600

    Update Mi_Asistente.py

--- Mi_Asistente.py
+++ Mi_Asistente.py
@@ -11,13 +11,13 @@ client = OpenAI(api_key=openai_api_key)
 
 # Create a session state variable to store the chat messages. This ensures that the
 # messages persist across reruns.
-# if "messages" not in st.session_state:
-    # st.session_state.messages = []
+if "messages" not in st.session_state:
+     st.session_state.messages = []
 
 # Display the existing chat messages via `st.chat_message`.
-#for message in st.session_state.messages:
- #   with st.chat_message(message["role"]):
-  #      st.markdown(message["content"])
+for message in st.session_state.messages:
+   with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
