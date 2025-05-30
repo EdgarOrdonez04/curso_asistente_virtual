00912cacfee53dc32c0aaefd39b864c73a7fed6d
EdgarOrdonez04 <eordonez@uach.mx>
2025-05-30 17:10:40 -0600

    Update Mi_Asistente.py

--- Mi_Asistente.py
+++ Mi_Asistente.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import fitz  # PyMuPDF para leer PDFs
 
 # Sidebar con información
-st.sidebar.title("🤖 💬 La Vieja Confiable AV")
+st.sidebar.title("🤖 💬 Asistente de Negocios por Internet")
 st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Escudo_UACH.svg/1200px-Escudo_UACH.svg.png")
 st.sidebar.write("Asistente elaborado por Edgar Francisco Ordoñez Bencomo")
 
